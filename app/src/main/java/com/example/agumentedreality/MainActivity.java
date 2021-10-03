7e0f35c53da28be64099dcb157ecf34374bba056
aydnyalcnn <61252776+aydnyalcnn@users.noreply.github.com>
2021-10-03 18:35:28 +0300

    FireBase entegrasyon sağlandı

--- app/src/main/java/com/example/agumentedreality/MainActivity.java
+++ app/src/main/java/com/example/agumentedreality/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.agumentedreality;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void grsclk(View view){
+
+
+    }
+    public void Kytclk(View view){
+
+
     }
 }
